44860a88dc36ced02c2e197ce3e5c2a6c6367a13
Jangyeonbin <75818592+Jangyeonbin@users.noreply.github.com>
2025-04-13 18:32:38 +0900

    Create empolyees.sql

---
+++ empolyees.sql
@@ -0,0 +1,15 @@
+CREATE TABLE empolyees (
+empolyees_number CHAR(3) PRIMARKY KEY,
+empolyees_name VARCHAR(20),
+empolyees_english_name VARCHAR(20),
+empolyees_position VARCHAR(10),
+empolyees_gender CHAR(2),
+empolyees_birthday DATE,
+Date_of_employment DATE,
+address VARCHAR(50),
+city VARCHAR(20),
+area VARCHAR(20)
+home_number VARCHAR(20),
+boss_number CHAR(3),
+department_number CHAR(2)
+);
