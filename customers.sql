695b9b341916960715f039ec793b7ccfb223bcc2
Jangyeonbin <75818592+Jangyeonbin@users.noreply.github.com>
2025-04-13 18:33:19 +0900

    Create customers.sql

---
+++ customers.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customers (
+customers_number CHAR PRIMARY KEY,
+customer_company VARCHAR(30),
+manager VARCHAR(20),
+manager_position VARCHAR(20),
+address VARCHAR(50),
+city VARCHAR(20),
+area VARCHAR(20),
+phone_number2 VARCHAR(20),
+mileage INT
+);
