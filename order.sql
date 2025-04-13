c16344f7c573f774fef7c8d9571dca206bb4eb29
Jangyeonbin <75818592+Jangyeonbin@users.noreply.github.com>
2025-04-13 18:32:06 +0900

    Create order.sql

---
+++ order.sql
@@ -0,0 +1,9 @@
+CREATE TABLE order(
+order_number CHAR(5) PRIMARY KEY,
+customers_number CHAR(5),
+empolyees_number CHAR(3),
+order_date DATE,
+request_date DATE,
+shipment_date DATE,
+order_location INT
+);
